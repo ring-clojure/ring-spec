28003797d802ade7ee9ac9fb0f0da776021152f5
James Reeves <jreeves@weavejester.com>
2016-10-04 16:23:31 +0100

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject ring-spec "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure specs for Ring"
+  :url "https://github.com/ring-clojure/ring-spec"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
