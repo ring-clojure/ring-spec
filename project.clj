af8a047264b5a1284e6ab59c1aa62fb8f7396a6d
James Reeves <jreeves@weavejester.com>
2016-10-04 15:38:42 +0100

    Update Clojure to 1.9.0-alpha13

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha12"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
                  [ring/ring-core "1.6.0-beta6"]]
   :profiles
   {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
