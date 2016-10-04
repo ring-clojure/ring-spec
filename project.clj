29a1ad860bf2b0c0ae0cf29f43b153c26f65e074
James Reeves <jreeves@weavejester.com>
2016-10-04 16:24:14 +0100

    Fix license information in project file

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject ring-spec "0.1.0-SNAPSHOT"
   :description "Clojure specs for Ring"
   :url "https://github.com/ring-clojure/ring-spec"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha13"]
                  [ring/ring-core "1.6.0-beta6"]]
   :profiles
